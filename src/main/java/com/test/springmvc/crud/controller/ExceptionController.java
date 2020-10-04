f4d63afb1cc78294c0df291f4de7ea80d71c21ca
罪恶羽翼 <875398515@qq.com>
2020-10-04 23:01:15 +0800

    add annotations

--- src/main/java/com/test/springmvc/crud/controller/ExceptionController.java
+++ src/main/java/com/test/springmvc/crud/controller/ExceptionController.java
@@ -3,9 +3,11 @@ package com.test.springmvc.crud.controller;
 import com.test.springmvc.crud.exception.MyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExceptionController {
